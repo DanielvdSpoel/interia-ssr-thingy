723a72c8b1f1b7144aeea4f214f1f4e919f74a4b
Daniel van der Spoel <daniel.van.der.spoel@politie.nl>
2024-05-31 21:22:25 +0200

    stuff

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,36 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/vue3';
+
+defineProps({
+    posts: {
+        type: Array,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <Head title="Dashboard"/>
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                {{ post.name }}
+                {{ post.body }}
+            </li>
+
+        </ul>
+    </AuthenticatedLayout>
+</template>
